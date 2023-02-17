e044c3d52814d4aef1d5fd8d4dabc42936c892e5
lonycell <lonycell@gmail.com>
2023-02-17 13:13:45 +0900

    v0.0.01-0000

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,7 @@
-// import { useEffect } from 'react'
-// import './App.css'
-
-// import api from './api/item'
-
-// function App() {
-//   useEffect(() => {
-//       api.characters().then( response => {
-//         console.log(response.data.data)      
-//       })
-//   }, [])
-
-//   return (
-//     <div className="App"></div>
-//   )
-// }
-
-import React, { Component, Suspense } from 'react'
+import React, { Suspense, useEffect } from 'react'
 import { HashRouter, Route, Routes } from 'react-router-dom'
+import api from './api/item'
+
 import './scss/style.scss'
 
 const loading = (
@@ -34,22 +19,26 @@ const Register = React.lazy(() => import('./views/pages/register/Register'))
 const Page404 = React.lazy(() => import('./views/pages/page404/Page404'))
 const Page500 = React.lazy(() => import('./views/pages/page500/Page500'))
 
-class App extends Component {
-  render() {
-    return (
-      <HashRouter>
-        <Suspense fallback={loading}>
-          <Routes>
-            <Route exact path="/login" name="Login Page" element={<Login />} />
-            <Route exact path="/register" name="Register Page" element={<Register />} />
-            <Route exact path="/404" name="Page 404" element={<Page404 />} />
-            <Route exact path="/500" name="Page 500" element={<Page500 />} />
-            <Route path="*" name="Home" element={<DefaultLayout />} />
-          </Routes>
-        </Suspense>
-      </HashRouter>
-    )
-  }
+const App = () => {
+  useEffect(() => {
+      api.characters().then( response => {
+        console.log(response.data.data)      
+      })
+  }, [])
+
+  return (
+    <HashRouter>
+      <Suspense fallback={loading}>
+        <Routes>
+          <Route exact path="/login" name="Login Page" element={<Login />} />
+          <Route exact path="/register" name="Register Page" element={<Register />} />
+          <Route exact path="/404" name="Page 404" element={<Page404 />} />
+          <Route exact path="/500" name="Page 500" element={<Page500 />} />
+          <Route path="*" name="Home" element={<DefaultLayout />} />
+        </Routes>
+      </Suspense>
+    </HashRouter>
+  )
 }
 
 export default App
--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,6 @@ import App from './App'
 import { Provider } from 'react-redux'
 import store from './store'
 
-import './index.css'
-
 ReactDOM.createRoot(document.getElementById('root')).render(
     <Provider store={store}>
         <App />
